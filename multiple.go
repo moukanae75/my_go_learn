0171be517df82701537661882fb1d45ff079ceba
moukanae75 <moukanae75@gmail.com>
2024-03-12 15:46:17 +0000

    miltipl

---
+++ multiple.go
@@ -0,0 +1,16 @@
+package main
+
+// import (
+// 	"fmt"
+// 	"os"
+// 	"strconv"
+// )
+// func main(){
+// 	arg :=os.Args[1]
+// 	var result int
+// 	div,_ :=strconv.Atoi(arg)
+// 	for i:=0;i<=10;i++{
+// 		result = div * i
+// 		fmt.Println(div ,"*",i,"=",result)
+// 	}
+// }
